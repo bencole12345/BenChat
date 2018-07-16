7acd05695fdaa5cec064f9e43e7433df8f4f536e
Ben Cole <bencole12345@gmail.com>
2018-07-16 11:48:39 +0100

    Correct documentation for LoggedInUser

--- app/src/main/java/pw/bencole/benchat/models/LoggedInUser.java
+++ app/src/main/java/pw/bencole/benchat/models/LoggedInUser.java
@@ -2,7 +2,7 @@ package pw.bencole.benchat.models;
 
 
 /**
- * Adds a password and id to a User.
+ * Adds a password to a User.
  *
  * This class should be used to represent the user that is actually using the app. The password
  * needs to be known in order to send requests to the API.
@@ -16,7 +16,7 @@ public class LoggedInUser extends User {
     public LoggedInUser(String username, String password, String userId) {
         super(username, userId);
         mPassword = password;
-    };
+    }
 
     public String getPassword() {
         return mPassword;
